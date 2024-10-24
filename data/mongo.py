3f2cd46c32cce0d59f5c41591f88483226129997
SalihiAdrian <bugsfromsalihiadrian@gmail.com>
2024-10-24 20:54:34 +0200

    Threw out experimental work, added personal attributes

--- data/mongo.py
+++ data/mongo.py
@@ -1,5 +1,12 @@
 import pymongo
 
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-mydb = myclient['social_database']
-mycol = mydb['people']
+def get_client():
+    return pymongo.MongoClient('mongodb://localhost:27017/')
+
+def get_social_database():
+    client = get_client()
+    return client['social_database']
+
+def get_people():
+    social_database = get_social_database()
+    return social_database['people']
---
+++ data/person_attributes.py
@@ -0,0 +1,82 @@
+person_attributes = {
+    "first_name": "",
+    "middle_name": "",
+    "last_name": "",
+    "birthday": "",
+    "gender": "",
+    "mobile_phone": "",
+    "email": "",
+    "street_and_number": "",
+    "postcode": "",
+    "city": "",
+    "country": "",
+    "first_contact": "",
+    "history_summary": "",
+
+    "likes": [],
+    "dislikes": [],
+    "wants": [],
+    "fears": [],
+    "confidence_level": "",
+    "openness": "",
+    "conscientiousness": "",
+    "extraversion": "",
+    "agreeableness": "",
+    "neuroticism": "",
+    "ambitiousness": "",
+    "education": "",
+    "subject_of_study": "",
+    "bad_mental_health_indicators": "",
+    "risk_tolerance": "",
+    "risk_seeking": "",
+    "goals": [],
+    "hobbies": [],
+
+    "bodylanguage": "",
+    "clothes_style": "",
+    "hygiene": "",
+    "sexual_orientation": "",
+    "studying_behaviour": "",
+    "loyalty_behaviour": "",
+    "current_job": "",
+    "job_history": "",
+    "political_strategies": "",
+    "projects_undertaken": "",
+    "successful_projects": "",
+    "failed_projects": "",
+    "social_style": "",
+    "base_trust": "",
+    "phone_usage": "",
+
+    "family_members": [],
+    "close_people": [],  # tuples
+    "main_caregiver_as_child": [],
+    "main_caregiver_behaviour": "",
+    "parents": [],
+    "parental_care": "",
+    "siblings": "",
+    "sibling_care": "",
+    "current_romantic_partner": [],
+    "romantic_ex_partners": [],
+    "type_of_romantic_relationships_in_past": [],
+    "type_of_romantic_relationships_in_present": [],
+    "close_friends": [],  # tuples, why and type of relationship, e.g. history and definition
+    "close_enemies": [],  # tuples, see above
+    "likes_these_people": [],  # tuple, person+rating 0-100, reason
+    "dislikes_these_people": [],  # tuple, person+rating 0-100, reason
+    "social_circles": [],
+    "close_friend_groups": [],  # tuple with name+list of ppl in the grouop
+    "relationship_with_family": "",
+    "frequency_of_visiting_family": "",
+    "likes_to_visit_family": "",
+    "current_crushes": [],
+    "possible_useful_people_for_person": [],  # tuple, person+reason
+    "person_possibly_useful_for_these_people": [],  # tuple, person+reason
+    "useful_people_already_known_to_person": [],  # tuple, person+reason+type of relationship
+    "reputation_in_each_social_circle": [],
+    "reputation_with_close_friends": "",
+    "specific_actions_in_past": [], # dictionary with: action, tactic, strategy that includes tactic, date employed, goal, success, guessed thought process, consequences
+    "predicted_future_actions": [],  # dict: what, assumed reason, already_occuring, likely_start, diligence_in_behaviour, end_date, predicted_consequences, how does this progress other goals?
+    "past_meetings": []  # dict of meetings: location, date, activity, summary
+
+}
